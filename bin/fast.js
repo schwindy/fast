e2333f81702ecb28468b5acb9ebee982fd353423
Robert Mariano Schwindaman <schwindy1234@gmail.com>
2017-09-21 22:46:31 -0600

    Fixed Windows Line Separators (zzz)

--- bin/fast.js
+++ bin/fast.js
@@ -11,12 +11,46 @@ const optionDefinitions = [
   { name: 'key', alias: 'k', type: String }
 ]
 
-const cliopts = commandLineArgs(optionDefinitions)
+const parseCliOptions = () => {
+  const cliopts = commandLineArgs(optionDefinitions)
 
-if (cliopts.init) {
-  init()
-} else if (cliopts.key) {
-  configure(cliopts.key)
-} else {
-  fast()
+  if (typeof cliopts !== 'object') {
+    console.error(`Fast::parseCliOptions(): Error: Invalid cliopts, expected object, got ${typeof cliopts}`, cliopts)
+    return {}
+  }
+
+  return cliopts
 }
+
+const cliopts = parseCliOptions()
+
+const boot = (args) => {
+  try {
+    if (cliopts.key) {
+      return configure(cliopts.key)
+    }
+
+    if (cliopts.init) {
+      return invoke('init', args)
+    }
+
+    return invoke('fast', args)
+  } catch (e) {
+    return console.error('Fast::boot(): Error: A fatal exception occurred!', e)
+  }
+}
+
+const invoke = (method, args) => {
+  try {
+    if (global[method] !== 'function') {
+      console.error(`Fast::invoke(${method}): Error: function ${method} is undefined!`)
+      return console.error('Fast::invoke(): Try reinstalling via: npm i -g .')
+    }
+
+    return global[method](args || false)
+  } catch (e) {
+    return console.error('Fast::invoke(): Error: A fatal exception occurred!', e)
+  }
+}
+
+boot()
