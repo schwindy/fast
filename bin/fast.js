8426296ba94d575a97bff753c1a26f09e00c2c2c
Robert Mariano Schwindaman <schwindy1234@gmail.com>
2017-09-21 20:18:06 -0600

    Merged :D

--- bin/fast.js
+++ bin/fast.js
@@ -2,29 +2,48 @@
 const commandLineArgs = require('command-line-args')
 const {
   lib,
-  fast
+  fast,
+  configure
 } = require('../lib')
 
 const optionDefinitions = [
-  {name: 'init', alias: 'i', type: Boolean}
+  { name: 'init', alias: 'i', type: Boolean },
+  { name: 'key', alias: 'k', type: String }
 ]
 
 const cliopts = commandLineArgs(optionDefinitions)
 
 const boot = (args) => {
   try {
+    if (cliopts.key) {
+      return configure(cliopts.key)
+    }
+
     if (cliopts.init) {
-      if (typeof init !== 'function') {
+      if (typeof global.init !== 'function') {
         console.error('Fast::boot(): Error: function init is undefined!')
         return console.error('Fast::boot(): Try reinstalling via: npm i -g .')
       }
 
-      return init(args)
+      return invoke('init', args)
     }
 
-    return fast(args)
+    return invoke('fast', args)
   } catch (e) {
     return console.error('Fast::boot(): Error: A fatal exception occurred!', e)
+  }
+}
+
+const invoke = (method, args) => {
+  try {
+    if (global[method] !== 'function') {
+      console.error(`Fast::invoke(${method}): Error: function ${method} is undefined!`)
+      return console.error('Fast::invoke(): Try reinstalling via: npm i -g .')
+    }
+
+    return global[method](args || false)
+  } catch (e) {
+    return console.error('Fast::invoke(): Error: A fatal exception occurred!', e)
   }
 }
 
