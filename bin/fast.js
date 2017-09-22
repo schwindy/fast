eb8c9fa2e29e07c00a4ee12cb34f07223ca0aecb
wski <contact.mattdylan@gmail.com>
2017-09-21 20:10:47 -0600

    key configuration

--- bin/fast.js
+++ bin/fast.js
@@ -2,17 +2,21 @@
 const commandLineArgs = require('command-line-args')
 const {
   lib,
-  fast
+  fast,
+  configure
 } = require('../lib')
 
 const optionDefinitions = [
-  { name: 'init', alias: 'i', type: Boolean }
+  { name: 'init', alias: 'i', type: Boolean },
+  { name: 'key', alias: 'k', type: String }
 ]
 
 const cliopts = commandLineArgs(optionDefinitions)
 
 if (cliopts.init) {
   init()
+} else if (cliopts.key) {
+  configure(cliopts.key)
 } else {
   fast()
 }
