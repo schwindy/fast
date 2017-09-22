dacb29572961d3369849ddde77140a272803a7c3
Robert Mariano Schwindaman <schwindy1234@gmail.com>
2017-09-21 20:12:17 -0600

    Fixed Tabbing (4 to 2)

--- bin/fast.js
+++ bin/fast.js
@@ -1,31 +1,31 @@
 #!/usr/bin/env node
 const commandLineArgs = require('command-line-args')
 const {
-    lib,
-    fast
+  lib,
+  fast
 } = require('../lib')
 
 const optionDefinitions = [
-    {name: 'init', alias: 'i', type: Boolean}
+  {name: 'init', alias: 'i', type: Boolean}
 ]
 
 const cliopts = commandLineArgs(optionDefinitions)
 
 const boot = (args) => {
-    try {
-        if (cliopts.init) {
-            if (typeof init !== 'function') {
-                console.error('Fast::boot(): Error: function init is undefined!')
-                return console.error('Fast::boot(): Try reinstalling via: npm i -g .')
-            }
+  try {
+    if (cliopts.init) {
+      if (typeof init !== 'function') {
+        console.error('Fast::boot(): Error: function init is undefined!')
+        return console.error('Fast::boot(): Try reinstalling via: npm i -g .')
+      }
 
-            return init(args)
-        }
-
-        return fast(args)
-    } catch (e) {
-        return console.error('Fast::boot(): Error: A fatal exception occurred!', e)
+      return init(args)
     }
+
+    return fast(args)
+  } catch (e) {
+    return console.error('Fast::boot(): Error: A fatal exception occurred!', e)
+  }
 }
 
 boot()
