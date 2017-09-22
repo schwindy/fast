6940fd7280eee28be0947680bf231592d49afd84
Robert Mariano Schwindaman <schwindy1234@gmail.com>
2017-09-21 20:10:24 -0600

    Added Fast::boot() and try-catch

--- bin/fast.js
+++ bin/fast.js
@@ -1,18 +1,31 @@
 #!/usr/bin/env node
 const commandLineArgs = require('command-line-args')
 const {
-  lib,
-  fast
+    lib,
+    fast
 } = require('../lib')
 
 const optionDefinitions = [
-  { name: 'init', alias: 'i', type: Boolean }
+    {name: 'init', alias: 'i', type: Boolean}
 ]
 
 const cliopts = commandLineArgs(optionDefinitions)
 
-if (cliopts.init) {
-  init()
-} else {
-  fast()
+const boot = (args) => {
+    try {
+        if (cliopts.init) {
+            if (typeof init !== 'function') {
+                console.error('Fast::boot(): Error: function init is undefined!')
+                return console.error('Fast::boot(): Try reinstalling via: npm i -g .')
+            }
+
+            return init(args)
+        }
+
+        return fast(args)
+    } catch (e) {
+        return console.error('Fast::boot(): Error: A fatal exception occurred!', e)
+    }
 }
+
+boot()
